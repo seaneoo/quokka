c0166f48b524e3f76edb17c7cbf896b34d962162
Sean O'Connor <so@seano.dev>
2024-10-01 19:20:01 -0600

    Exclude UserDetailsServiceAutoConfiguration from application
    This disables the pre-generated Spring Security user

--- src/main/java/dev/seano/quokka/QuokkaApplication.java
+++ src/main/java/dev/seano/quokka/QuokkaApplication.java
@@ -2,8 +2,9 @@ package dev.seano.quokka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {UserDetailsServiceAutoConfiguration.class})
 public class QuokkaApplication {
 
 	public static void main(String[] args) {
