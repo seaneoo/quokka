f8671dff10bd988cb4c9eed0a516c83b7257b10a
Sean O'Connor <so@seano.dev>
2024-10-01 19:58:58 -0600

    Update SecurityConfig
    Temporarily re-enable HTTP Basic. This will be used until we can implement our own, more secure auth filter.

--- src/main/java/dev/seano/quokka/security/SecurityConfig.java
+++ src/main/java/dev/seano/quokka/security/SecurityConfig.java
@@ -2,12 +2,12 @@ package dev.seano.quokka.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.CorsConfigurer;
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.config.annotation.web.configurers.FormLoginConfigurer;
-import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -18,9 +18,10 @@ public class SecurityConfig {
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		// TODO 2024-10-01, 19:16 Configure CORS and CSRF before production release
+		// TODO 2024-10-01, 19:56 Disable httpBasic and implement custom filter
 		http.cors(CorsConfigurer::disable)
 			.csrf(CsrfConfigurer::disable)
-			.httpBasic(HttpBasicConfigurer::disable)
+			.httpBasic(Customizer.withDefaults())
 			.formLogin(FormLoginConfigurer::disable)
 			.authorizeHttpRequests(authorizeRequests -> authorizeRequests.anyRequest().permitAll())
 			.sessionManagement(
