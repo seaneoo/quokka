75f356ff8092e9e2ed80fde7300992ce80438d3b
Sean O'Connor <so@seano.dev>
2024-10-01 19:39:05 -0600

    Create User entity and repository

---
+++ src/main/java/dev/seano/quokka/user/User.java
@@ -0,0 +1,56 @@
+package dev.seano.quokka.user;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnTransformer;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
+
+@Table(name = "users")
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User implements UserDetails {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.UUID)
+	private UUID id;
+
+	@Column(unique = true, nullable = false, length = 20)
+	@ColumnTransformer(write = "LOWER(?)")
+	private String username;
+
+	@Column(nullable = false)
+	private String password;
+
+	@Column(nullable = false, updatable = false)
+	@CreatedDate
+	private Date created;
+
+	@Column(nullable = false)
+	@LastModifiedDate
+	private Date lastModified;
+
+	@Column(nullable = false)
+	@Builder.Default
+	private boolean enabled = true;
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return List.of();
+	}
+}
