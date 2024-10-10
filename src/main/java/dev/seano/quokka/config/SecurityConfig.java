85c66aeded93c8898c198c8b762b9935f5699889
Sean O'Connor <so@seano.dev>
2024-10-10 07:10:34 -0600

    Move PasswordEncoder bean to SecurityConfig

--- src/main/java/dev/seano/quokka/config/SecurityConfig.java
+++ src/main/java/dev/seano/quokka/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package dev.seano.quokka.config;
 
+import dev.seano.quokka.ApplicationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -9,11 +10,19 @@ import org.springframework.security.config.annotation.web.configurers.CorsConfig
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.config.annotation.web.configurers.FormLoginConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableMethodSecurity
 public class SecurityConfig {
+
+	private final ApplicationProperties applicationProperties;
+
+	public SecurityConfig(ApplicationProperties applicationProperties) {
+		this.applicationProperties = applicationProperties;
+	}
 
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -30,5 +39,12 @@ public class SecurityConfig {
 			.sessionManagement(
 				sessionManagement -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 		return http.build();
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new Argon2PasswordEncoder(applicationProperties.getArgon2().getSaltLength(),
+			applicationProperties.getArgon2().getHashLength(), applicationProperties.getArgon2().getParallelism(),
+			applicationProperties.getArgon2().getMemory(), applicationProperties.getArgon2().getIterations());
 	}
 }
--- src/main/java/dev/seano/quokka/feature/auth/AuthConfig.java
+++ src/main/java/dev/seano/quokka/feature/auth/AuthConfig.java
@@ -1,11 +1,9 @@
 package dev.seano.quokka.feature.auth;
 
-import dev.seano.quokka.ApplicationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
@@ -13,25 +11,18 @@ public class AuthConfig {
 
 	private final CustomUserDetailsService userDetailsService;
 
-	private final ApplicationProperties applicationProperties;
+	private final PasswordEncoder passwordEncoder;
 
-	public AuthConfig(CustomUserDetailsService userDetailsService, ApplicationProperties applicationProperties) {
+	public AuthConfig(CustomUserDetailsService userDetailsService, PasswordEncoder passwordEncoder) {
 		this.userDetailsService = userDetailsService;
-		this.applicationProperties = applicationProperties;
-	}
-
-	@Bean
-	public PasswordEncoder passwordEncoder() {
-		return new Argon2PasswordEncoder(applicationProperties.getArgon2().getSaltLength(),
-			applicationProperties.getArgon2().getHashLength(), applicationProperties.getArgon2().getParallelism(),
-			applicationProperties.getArgon2().getMemory(), applicationProperties.getArgon2().getIterations());
+		this.passwordEncoder = passwordEncoder;
 	}
 
 	@Bean
 	public AuthenticationProvider authenticationProvider() {
 		var authenticationProvider = new DaoAuthenticationProvider();
 		authenticationProvider.setUserDetailsService(userDetailsService);
-		authenticationProvider.setPasswordEncoder(passwordEncoder());
+		authenticationProvider.setPasswordEncoder(passwordEncoder);
 		return authenticationProvider;
 	}
 }
