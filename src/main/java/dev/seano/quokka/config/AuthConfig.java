7e8255a5d0bf7c7a8e75ab6646fa3c93215de358
Sean O'Connor <so@seano.dev>
2024-10-10 07:10:48 -0600

    Move to top-level config package

--- src/main/java/dev/seano/quokka/config/AuthConfig.java
+++ src/main/java/dev/seano/quokka/config/AuthConfig.java
@@ -1,5 +1,6 @@
-package dev.seano.quokka.feature.auth;
+package dev.seano.quokka.config;
 
+import dev.seano.quokka.feature.auth.CustomUserDetailsService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
