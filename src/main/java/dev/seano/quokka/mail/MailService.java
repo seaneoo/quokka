2535ab4c75be4bb772d98e0c09fbe4b00e693d1f
Sean O'Connor <so@seano.dev>
2024-10-11 06:32:31 -0600

    Add MailService::sendWithRetry
    This allows us to send a "safe" email. If an exception is thrown, the error is logged, and it attempts to resend the email.

--- src/main/java/dev/seano/quokka/mail/MailService.java
+++ src/main/java/dev/seano/quokka/mail/MailService.java
@@ -2,7 +2,9 @@ package dev.seano.quokka.mail;
 
 import dev.seano.quokka.ApplicationProperties;
 import dev.seano.quokka.feature.user.UserEntity;
+import jakarta.mail.MessagingException;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.mail.MailSendException;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.scheduling.annotation.Async;
@@ -22,22 +24,40 @@ public class MailService {
 	}
 
 	@Async
-	public void send(String to, String subject, String message) {
-		try {
-			var mailMessage = mailSender.createMimeMessage();
-			mailMessage.setSubject(subject);
-			var helper = new MimeMessageHelper(mailMessage, true);
-			helper.setFrom(applicationProperties.getMail().getFrom());
-			helper.setTo(to);
-			helper.setText(message, true);
-			mailSender.send(mailMessage);
-		} catch (Exception e) {
-			log.warn("Could not send email", e);
+	public void send(String to, String subject, String message) throws MessagingException {
+		log.debug("Attempting to send an email to '{}'", to);
+		var mailMessage = mailSender.createMimeMessage();
+		mailMessage.setSubject(subject);
+		var helper = new MimeMessageHelper(mailMessage, true);
+		helper.setFrom(applicationProperties.getMail().getFrom());
+		helper.setTo(to);
+		helper.setText(message, true);
+		mailSender.send(mailMessage);
+		log.debug("Successfully sent email to '{}'", to);
+	}
+
+	@Async
+	public void sendWithRetry(String to, String subject, String message) {
+		var retries = 3;
+		var attempts = 0;
+		while (attempts < retries) {
+			try {
+				send(to, subject, message);
+				return;
+			} catch (MailSendException e) {
+				attempts++;
+				if (attempts >= retries) {
+					log.warn("Could not send email to '{}' after {} attempts", to, attempts, e);
+				}
+			} catch (Exception e) {
+				log.warn("Messaging error when sending email to '{}' - will not attempt again", to, e);
+				break;
+			}
 		}
 	}
 
 	@Async
 	public void send(UserEntity user, String subject, String message) {
-		send(user.getEmail(), subject, message);
+		sendWithRetry(user.getEmail(), subject, message);
 	}
 }
