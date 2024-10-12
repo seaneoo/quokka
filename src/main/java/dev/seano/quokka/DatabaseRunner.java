46f5b435babd1cd47a773b29ec1b77cb9a16eca4
Sean O'Connor <so@seano.dev>
2024-10-11 19:19:53 -0600

    Update example users
    Created example "admin" user.

--- src/main/java/dev/seano/quokka/DatabaseRunner.java
+++ src/main/java/dev/seano/quokka/DatabaseRunner.java
@@ -1,30 +1,38 @@
 package dev.seano.quokka;
 
+import dev.seano.quokka.feature.user.Role;
 import dev.seano.quokka.feature.user.User;
-import dev.seano.quokka.feature.user.UserService;
+import dev.seano.quokka.feature.user.UserRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
 @Slf4j
 public class DatabaseRunner implements CommandLineRunner {
 
-	private final UserService userService;
+	private final UserRepository userRepository;
 
 	private final PasswordEncoder passwordEncoder;
 
-	public DatabaseRunner(UserService userService, PasswordEncoder passwordEncoder) {
-		this.userService = userService;
+	public DatabaseRunner(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+		this.userRepository = userRepository;
 		this.passwordEncoder = passwordEncoder;
 	}
 
 	@Override
 	public void run(String... args) {
 		try {
-			var user = User.builder().username("user").password(passwordEncoder.encode("changeme")).build();
-			userService.save(user);
+			var user = User.builder().username("ex_user").password(passwordEncoder.encode("changeme")).build();
+			var admin = User.builder()
+				.username("ex_admin")
+				.password(passwordEncoder.encode("changeme"))
+				.role(Role.ADMIN)
+				.build();
+			userRepository.saveAll(List.of(user, admin));
 		} catch (Exception e) {
 			log.warn(e.getMessage());
 		}
