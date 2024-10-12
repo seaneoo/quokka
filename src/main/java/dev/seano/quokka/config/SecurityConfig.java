07c458681f86232823df0b4316f3f9cf32654293
Sean O'Connor <so@seano.dev>
2024-10-11 19:26:26 -0600

    Reformat code

--- src/main/java/dev/seano/quokka/config/SecurityConfig.java
+++ src/main/java/dev/seano/quokka/config/SecurityConfig.java
@@ -18,33 +18,36 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableMethodSecurity
 public class SecurityConfig {
 
-	private final ApplicationProperties applicationProperties;
+    private final ApplicationProperties applicationProperties;
 
-	public SecurityConfig(ApplicationProperties applicationProperties) {
-		this.applicationProperties = applicationProperties;
-	}
+    public SecurityConfig(ApplicationProperties applicationProperties) {
+        this.applicationProperties = applicationProperties;
+    }
 
-	@Bean
-	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-		// TODO 2024-10-01, 19:16 Configure CORS and CSRF before production release
-		// TODO 2024-10-01, 19:56 Disable httpBasic and implement custom filter
-		http.cors(CorsConfigurer::disable)
-			.csrf(CsrfConfigurer::disable)
-			.httpBasic(Customizer.withDefaults())
-			.formLogin(FormLoginConfigurer::disable)
-			.authorizeHttpRequests(authorizeRequests -> authorizeRequests.requestMatchers("/user/**")
-				.authenticated()
-				.anyRequest()
-				.permitAll())
-			.sessionManagement(
-				sessionManagement -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-		return http.build();
-	}
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        // TODO 2024-10-01, 19:16 Configure CORS and CSRF before production release
+        // TODO 2024-10-01, 19:56 Disable httpBasic and implement custom filter
+        http.cors(CorsConfigurer::disable)
+                .csrf(CsrfConfigurer::disable)
+                .httpBasic(Customizer.withDefaults())
+                .formLogin(FormLoginConfigurer::disable)
+                .authorizeHttpRequests(authorizeRequests -> authorizeRequests.requestMatchers("/user/**")
+                        .authenticated()
+                        .anyRequest()
+                        .permitAll())
+                .sessionManagement(
+                        sessionManagement -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        return http.build();
+    }
 
-	@Bean
-	public PasswordEncoder passwordEncoder() {
-		return new Argon2PasswordEncoder(applicationProperties.getArgon2().getSaltLength(),
-			applicationProperties.getArgon2().getHashLength(), applicationProperties.getArgon2().getParallelism(),
-			applicationProperties.getArgon2().getMemory(), applicationProperties.getArgon2().getIterations());
-	}
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new Argon2PasswordEncoder(applicationProperties.getArgon2()
+                .getSaltLength(), applicationProperties.getArgon2()
+                .getHashLength(), applicationProperties.getArgon2()
+                .getParallelism(), applicationProperties.getArgon2()
+                .getMemory(), applicationProperties.getArgon2()
+                .getIterations());
+    }
 }
